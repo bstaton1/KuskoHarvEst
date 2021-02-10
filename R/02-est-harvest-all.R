a2b06543dee13dadef78c7730589e691d7465168
Ben Staton <statonbe@gmail.com>
2021-02-09 21:09:47 -0800

    Create an estimate_harvest_all() function
    Estimates harvest for all strata

---
+++ R/02-est-harvest-all.R
@@ -0,0 +1,43 @@
+#' Estimate Harvest for All Strata
+#'
+#' @export
+
+estimate_harvest_all = function(interview_data, effort_info, gear, stratify_interviews = TRUE) {
+
+  # extract the records for this gear type
+  interview_data = interview_data[interview_data$gear == gear,]
+
+  # extract the strata names
+  strata_names = names(effort_info$effort_est_stratum)
+
+  if (!stratify_interviews) {
+    # apply the estimate_harvest() function separately to data from each stratum, but use all interview data
+    ests_all = sapply(strata_names, function(s) {
+      estimate_harvest(interview_data = interview_data,
+                       effort_est = effort_info$effort_est_stratum[s],
+                       gear = gear
+      )
+    })
+  } else {
+    # apply the estimate_harvest() function separately to data from each stratum, but use strata-specific interview data
+    ests_all = sapply(strata_names, function(s) {
+      estimate_harvest(interview_data = interview_data[interview_data$stratum == s,],
+                       effort_est = effort_info$effort_est_stratum[s],
+                       gear = gear
+      )
+    })
+  }
+
+  # format the output: add strata and species totals
+  output = t(ests_all)
+  output = rbind(output, total = colSums(output))
+  output = cbind(output, total = rowSums(output))
+
+  # format output: give gear/stratum IDs
+  output = data.frame(output)
+  output = cbind(gear = gear, stratum = rownames(output), output)
+  rownames(output) = NULL
+
+  # return the output
+  return(output)
+}
