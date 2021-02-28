899113d95fea2378b557abe245e4d14561e6695d
Ben Staton <statonbe@gmail.com>
2021-02-28 10:26:51 -0800

    Build the prepare_interviews_all() function

---
+++ R/01-data-interviews-all.R
@@ -0,0 +1,38 @@
+#' Prepare all interview data
+#'
+#' @export
+
+prepare_interviews_all = function(input_files, ...) {
+  # read in and format raw interview data
+  interview_data_list = lapply(input_files, function(file) prepare_interviews(file, ...))
+
+  # combine individual list elements into a data frame
+  interview_data = unlist_dfs(interview_data_list)
+
+  # perform suitability checks and combine logical flags with the data
+  tasks = c("nothing", "effort", "catch_rate_info", "catch_rate_info_reliable", "avg_soak", "avg_net_length")
+  suitable = sapply(tasks, suitable_for, interview_data = interview_data)
+  colnames(suitable) = paste0("suit_", c("nothing", "effort", "cr_info", "cr_reliable", "avg_soak", "avg_net"))
+  interview_data = cbind(interview_data, suitable)
+
+  # perform checks for impossible soak times
+  # if any are found, change the soak time to be the same as the trip duration, and include a note
+  interview_data$note = NA
+  impossible_soaks = !is_possible_soak(interview_data)
+  if (any(impossible_soaks)) {
+    interview_data$note[impossible_soaks] = paste0("Impossible soak duration (", interview_data[impossible_soaks,"soak_duration"], ") edited to trip duration")
+    interview_data$soak_duration[impossible_soaks] = interview_data$trip_duration[impossible_soaks]
+    warning(sum(impossible_soaks), " interview(s) had soak duration reported longer than trip duration.\nFor these records, the soak duration has been set to the trip duration,\nand a note has been included in the output.")
+  }
+
+  # perform checks for impossible trip times
+  impossible_trips = !is_possible_soak(interview_data)
+  if (any(impossible_trips)) {
+    interview_data$note[impossible_trips] = paste0("Impossible trip times reported, record is excluded from effort estimation.")
+    interview_data$suit_effort[impossible_trips] = FALSE
+    warning(sum(impossible_trips), " interview(s) had impossible trip times reported.\nFor these records, the trip times will not be used in effort estimation,\nand a note has been included in the output.")
+  }
+
+  # return the output
+  return(interview_data)
+}
