dfe465559467a657069163b956f7242aabcf6ef0
Ben Staton <statonbe@gmail.com>
2021-03-03 20:51:09 -0800

    Update bootstrap_harvest to work with other new functions

--- R/02-est-bootstrap.R
+++ R/02-est-bootstrap.R
@@ -2,14 +2,18 @@
 #'
 #' @export
 
-bootstrap_harvest = function(interview_data, effort_info, gear, n_boot = 1000, source_weights = NULL, stratify_interviews = TRUE) {
+bootstrap_harvest = function(interview_data, effort_info, gear, n_boot = 1000, stratify_interviews = TRUE, seed = 1234) {
+
+  # set the random seed
+  set.seed(seed)
 
   # loop over bootstrap iterations, randomizing the data, and obtaining all harvest estimates for each
   output = lapply(1:n_boot, function(i) {
-    out = estimate_harvest_all(
-      interview_data = randomize_data(interview_data[interview_data$gear == gear,], source_weights = source_weights),
+    out = estimate_harvest(
+      interview_data = interview_data,
       effort_info = effort_info,
       gear = gear,
+      randomize = TRUE,
       stratify_interviews = stratify_interviews
     )
     out = cbind(iter = i, out)
@@ -18,4 +22,7 @@ bootstrap_harvest = function(interview_data, effort_info, gear, n_boot = 1000, s
 
   # combine the output of each bootstrapped sample into a data frame
   output = unlist_dfs(output)
+
+  # return the output
+  return(output)
 }
