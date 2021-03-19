b08fa5ad3cef2dcb3043c08eb6314144b8c311f2
Ben Staton <statonbe@gmail.com>
2021-03-18 19:32:55 -0700

    Handle impossible soaks/trips better
    Mostly better warning messages. But also, force the trip times and duration to be NA if it is impossible

--- R/01-data-interviews-all.R
+++ R/01-data-interviews-all.R
@@ -25,22 +25,25 @@ prepare_interviews_all = function(input_files, ...) {
     warning("\n", sum(no_gear), " interview(s) had missing gear type information.\nThese records have been discarded since they\ncannot be used for anything.")
   }
 
+  # perform checks for impossible trip times
+  interview_data$note = NA
+  impossible_trips = !is_possible_trip(interview_data)
+  if (any(impossible_trips)) {
+    interview_data$note[impossible_trips] = paste0("Impossible trip times reported, trip times set to NA.")
+    interview_data$suit_effort[impossible_trips] = FALSE
+    interview_data$trip_duration[impossible_trips] = NA
+    interview_data$trip_start[impossible_trips] = NA
+    interview_data$trip_end[impossible_trips] = NA
+    warning("\n", sum(impossible_trips), " interview(s) had impossible trip times reported.\nFor these records, the trip times have been set to NA,\nand a note has been included in the output.")
+  }
+
   # perform checks for impossible soak times
   # if any are found, change the soak time to be the same as the trip duration, and include a note
-  interview_data$note = NA
   impossible_soaks = !is_possible_soak(interview_data)
   if (any(impossible_soaks)) {
     interview_data$note[impossible_soaks] = paste0("Impossible soak duration (", interview_data[impossible_soaks,"soak_duration"], ") edited to trip duration")
     interview_data$soak_duration[impossible_soaks] = interview_data$trip_duration[impossible_soaks]
-    warning(sum(impossible_soaks), " interview(s) had soak duration reported longer than trip duration.\nFor these records, the soak duration has been set to the trip duration,\nand a note has been included in the output.")
-  }
-
-  # perform checks for impossible trip times
-  impossible_trips = !is_possible_soak(interview_data)
-  if (any(impossible_trips)) {
-    interview_data$note[impossible_trips] = paste0("Impossible trip times reported, record is excluded from effort estimation.")
-    interview_data$suit_effort[impossible_trips] = FALSE
-    warning(sum(impossible_trips), " interview(s) had impossible trip times reported.\nFor these records, the trip times will not be used in effort estimation,\nand a note has been included in the output.")
+    warning("\n", sum(impossible_soaks), " interview(s) had soak duration reported longer than trip duration.\nFor these records, the soak duration has been set to the trip duration,\nand a note has been included in the output.")
   }
 
   # return the output
