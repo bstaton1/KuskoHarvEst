1448c93af94e57746dc96c726aecd38146fe60ab
Ben <statonbe@gmail.com>
2023-04-29 08:40:43 -0700

    Create `combine_boot_out()` function

--- R/02-est-bootstrap.R
+++ R/02-est-bootstrap.R
@@ -34,3 +34,61 @@ bootstrap_harvest = function(interview_data, effort_info, gear, n_boot = 1000, s
   # return the output
   return(output)
 }
+
+#' Combine Bootstrap Harvest Estimates Across Gear Types
+#'
+#' Accepts drift and set net bootstrap output from [bootstrap_harvest()]
+#' And returns a combined data frame including a total across gears.
+#'
+#' @param boot_out_drift Output from running [bootstrap_harvest()] with `gear = "drift"`.
+#'   Leave to default (`NULL`) if no drift net harvest estimate made
+#' @param boot_out_set Output from running [bootstrap_harvest()] with `gear = "set"`
+#'   Leave to default (`NULL`) if no set net harvest estimate made
+#' @details If no estimates are available for a given gear type, then placeholder `NA` values will be
+#'   created in the output.
+#' @export
+
+combine_boot_out = function(boot_out_drift = NULL, boot_out_set = NULL) {
+
+  # create shortcut variables
+  no_drift = is.null(boot_out_drift)
+  no_set = is.null(boot_out_set)
+
+  # if both inputs are null, return error
+  if (no_drift & no_set) {
+    stop ("At least one of 'boot_out_drift' or 'boot_out_set' must contain the output of KuskoHarvEst::bootstrap_harvest()")
+  }
+
+  # which columns store ID variables
+  id_vars = c("iter", "date", "gear", "stratum")
+
+  # placeholder object for if no drift estimates
+  # duplicate the set net output and convert estimates to NA
+  if (no_drift) {
+    boot_out_drift = boot_out_set
+    boot_out_drift[,!(colnames(boot_out_drift) %in% id_vars)] = NA
+    boot_out_drift$gear = "drift"
+  }
+
+  # placeholder object for if no set estimates
+  # duplicate the drift net output and convert estimates to NA
+  if (no_set) {
+    boot_out_set = boot_out_drift
+    boot_out_set[,!(colnames(boot_out_set) %in% id_vars)] = NA
+    boot_out_set$gear = "set"
+  }
+
+  # extract species names
+  spp = colnames(boot_out_drift)[!(colnames(boot_out_drift) %in% id_vars)]
+
+  # obtain a total across gears
+  boot_out_total = cbind(
+    boot_out_drift[,id_vars],
+    sapply(spp, function(s) rowSums(cbind(boot_out_drift[,s], boot_out_set[,s]), na.rm = TRUE))
+  )
+  boot_out_total$gear = "total"
+
+  # combine gear types
+  rbind(boot_out_total, boot_out_drift, boot_out_set)
+
+}
