6ed7a367267ad9f1e0ec04633d0badcc2f8bd4b1
Ben <statonbe@gmail.com>
2023-04-29 13:10:33 -0700

    Create a species_names internal table
    See #196

--- data-raw/make-internal-tables.R
+++ data-raw/make-internal-tables.R
@@ -8,5 +8,13 @@ strata_names = data.frame(stratum = c("A", "B", "C", "D1"),
                           stratum_start = c("Tuntutuliak", "Johnson R.", "Napaskiak", "Akiachak"),
                           stratum_end = c("Johnson R.", "Napaskiak", "Akiachak", "Akiak"))
 
+# make a table specifying which species the program should accept
+salmon_spp = c("chinook", "chum", "sockeye")
+nonsalmon_spp = c("whitefish", "sheefish")
+spp = c(salmon_spp, nonsalmon_spp)
+is_salmon = spp %in% salmon_spp
+in_text = ifelse(spp == "chinook", KuskoHarvUtils::capitalize(spp), spp)
+species_names = data.frame(spp = spp, is_salmon = is_salmon, in_text = in_text)
+
 # export them to proper structure and location
-usethis::use_data(source_names, strata_names, internal = TRUE)
+usethis::use_data(source_names, strata_names, species_names, internal = TRUE, overwrite = TRUE)
