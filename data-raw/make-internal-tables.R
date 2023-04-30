b0d62c2943ab5747286614710c5bcb25b5b5018c
Ben <statonbe@gmail.com>
2023-04-29 22:15:24 -0700

    Small typo in species_names table

--- data-raw/make-internal-tables.R
+++ data-raw/make-internal-tables.R
@@ -9,12 +9,12 @@ strata_names = data.frame(stratum = c("A", "B", "C", "D1"),
                           stratum_end = c("Johnson R.", "Napaskiak", "Akiachak", "Akiak"))
 
 # make a table specifying which species the program should accept
-salmon_spp = c("chinook", "chum", "sockeye")
-nonsalmon_spp = c("whitefish", "sheefish")
-spp = c(salmon_spp, nonsalmon_spp)
-is_salmon = spp %in% salmon_spp
-in_text = ifelse(spp == "chinook", KuskoHarvUtils::capitalize(spp), spp)
-species_names = data.frame(spp = spp, is_salmon = is_salmon, in_text = in_text)
+salmon_species = c("chinook", "chum", "sockeye")
+nonsalmon_species = c("whitefish", "sheefish")
+species = c(salmon_species, nonsalmon_species)
+is_salmon = species %in% salmon_species
+in_text = ifelse(species == "chinook", KuskoHarvUtils::capitalize(species), species)
+species_names = data.frame(species = species, is_salmon = is_salmon, in_text = in_text)
 
 # export them to proper structure and location
 usethis::use_data(source_names, strata_names, species_names, internal = TRUE, overwrite = TRUE)
