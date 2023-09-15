0b66b6bff88028f083a897f0903e8cb5bcade01f
Ben <statonbe@gmail.com>
2023-09-15 07:29:36 -0700

    Update scripts that produce all post-season report figures and tables

--- inst/rstudio/templates/07-final-report-content/03-make-all-figures.R
+++ inst/rstudio/templates/07-final-report-content/03-make-all-figures.R
@@ -40,31 +40,31 @@ p = apply(ests[,strata], 1, function(x) x/sum(x, na.rm = TRUE))
 p[is.na(p)] = 0
 
 # which bar to label with stratum labels
-label_bar_A = 2
-label_bar_B = 2
-label_bar_C = 2
-label_bar_D1 = 2
-label_bar_D2 = 2
+label_bar_A = 1
+label_bar_B = 1
+label_bar_C = 1
+label_bar_D1 = 1
+label_bar_D2 = 1
 
 # open a graphics device
 png(file.path(out_dir, "driftnet-effort-by-opener.png"), h = 5 * ppi, w = 7 * ppi, res = ppi)
 
 # set graphics parameters
-par(mfrow = c(1,2), mar = c(2,2,1,1), oma = c(0,2,0,2), xaxs = "i", yaxs = "i", tcl = -0.15, mgp = c(2,0.35,0))
+par(mfrow = c(1,2), mar = c(2,1.5,1,0.5), oma = c(0,2,0,2), xaxs = "i", yaxs = "i", tcl = -0.15, mgp = c(2,0.35,0))
 
 # make barplot: total effort estimate by date
-mp = barplot(ests$total, ylim = c(0, max(ests$total, na.rm = TRUE)), las = 1, col = "grey75", space = 0.1)
+mp = barplot(ests$total, ylim = c(0, max(ests$total, na.rm = TRUE)), las = 1, col = "grey75", space = 0)
 usr = par("usr")
 segments(usr[1], usr[3], usr[2], usr[3], xpd = TRUE)
-axis(side = 1, at = mp, labels = dates, las = 2)
+axis(side = 1, at = mp, labels = dates, las = 2, cex.axis = 0.8)
 
 # make stacked barplot: proportion of effort in each stratum by date
-par(mar = c(2,1,1,2))
-mp = barplot(p, beside = F, ylim = c(0,1), col = c("grey40", "grey60", "grey80", "grey90", "white"), space = 0.1, yaxt = "n", xaxt = "n")
+par(mar = c(2,0.5,1,1.5))
+mp = barplot(p, beside = F, ylim = c(0,1), col = c("grey40", "grey60", "grey80", "grey90", "white"), space = 0, yaxt = "n", xaxt = "n")
 usr = par("usr")
 axis(side = 4, at = seq(0,1,0.2), labels = seq(0,1,0.2), las = 2)
 segments(usr[1], usr[3], usr[2], usr[3], xpd = TRUE)
-axis(side = 1, at = mp, labels = dates, las = 2)
+axis(side = 1, at = mp, labels = dates, las = 2, cex.axis = 0.8)
 
 # label the bar with stratum
 text(x = mp[label_bar_A], y = p[1,label_bar_A]/2, labels = "A", font = 2)
@@ -74,8 +74,8 @@ text(x = mp[label_bar_D1], y = (sum(p[1:4,label_bar_D1]) + sum(p[1:3,label_bar_D
 text(x = mp[label_bar_D2], y = (sum(p[1:5,label_bar_D2]) + sum(p[1:4,label_bar_D2]))/2, labels = "D2", font = 2)
 
 # add yaxis labels
-mtext(side = 2, outer = T, line = 0.5, "Total Estimated Drift Boat Trips")
-mtext(side = 4, outer = T, line = 0.5, "Proportion of Drift Boat Trips Per Stratum")
+mtext(side = 2, outer = T, line = 0.75, "Total Estimated Drift Boat Trips")
+mtext(side = 4, outer = T, line = 0.75, "Proportion of Drift Boat Trips Per Stratum")
 
 # close the graphics device
 dev.off(); file.show(file.path(out_dir, "driftnet-effort-by-opener.png"))
@@ -110,21 +110,21 @@ label_bar_D2 = 1
 png(file.path(out_dir, "setnet-effort-by-opener.png"), h = 5 * ppi, w = 7 * ppi, res = ppi)
 
 # set graphics parameters
-par(mfrow = c(1,2), mar = c(2,2,1,1), oma = c(0,2,0,2), xaxs = "i", yaxs = "i", tcl = -0.15, mgp = c(2,0.35,0))
+par(mfrow = c(1,2), mar = c(2,1.5,1,0.5), oma = c(0,2,0,2), xaxs = "i", yaxs = "i", tcl = -0.15, mgp = c(2,0.35,0))
 
 # make barplot: total effort estimate by date
-mp = barplot(ests$total, ylim = c(0, max(ests$total, na.rm = TRUE)), las = 1, col = "grey75", space = 0.1)
+mp = barplot(ests$total, ylim = c(0, max(ests$total, na.rm = TRUE)), las = 1, col = "grey75", space = 0)
 usr = par("usr")
 segments(usr[1], usr[3], usr[2], usr[3], xpd = TRUE)
-axis(side = 1, at = mp, labels = dates, las = 2)
+axis(side = 1, at = mp, labels = dates, las = 2, cex.axis = 0.8)
 
 # make stacked barplot: proportion of effort in each stratum by date
-par(mar = c(2,1,1,2))
-mp = barplot(p, beside = F, ylim = c(0,1), col = c("grey40", "grey60", "grey80", "grey90", "white"), space = 0.1, yaxt = "n", xaxt = "n")
+par(mar = c(2,0.5,1,1.5))
+mp = barplot(p, beside = F, ylim = c(0,1), col = c("grey40", "grey60", "grey80", "grey90", "white"), space = 0, yaxt = "n", xaxt = "n")
 usr = par("usr")
 axis(side = 4, at = seq(0,1,0.2), labels = seq(0,1,0.2), las = 2)
 segments(usr[1], usr[3], usr[2], usr[3], xpd = TRUE)
-axis(side = 1, at = mp, labels = dates, las = 2)
+axis(side = 1, at = mp, labels = dates, las = 2, cex.axis = 0.8)
 
 # label the bar with stratum
 text(x = mp[label_bar_A], y = p[1,label_bar_A]/2, labels = "A", font = 2)
@@ -134,8 +134,8 @@ text(x = mp[label_bar_D1], y = (sum(p[1:4,label_bar_D1]) + sum(p[1:3,label_bar_D
 text(x = mp[label_bar_D2], y = (sum(p[1:5,label_bar_D2]) + sum(p[1:4,label_bar_D2]))/2, labels = "D2", font = 2)
 
 # add yaxis labels
-mtext(side = 2, outer = T, line = 0.5, "Total Estimated Set Net Trips")
-mtext(side = 4, outer = T, line = 0.5, "Proportion of Set Net Trips Per Stratum")
+mtext(side = 2, outer = T, line = 0.75, "Total Estimated Set Net Trips")
+mtext(side = 4, outer = T, line = 0.75, "Proportion of Set Net Trips Per Stratum")
 
 # close the graphics device
 dev.off(); file.show(file.path(out_dir, "setnet-effort-by-opener.png"))
@@ -168,21 +168,21 @@ label_bar_FC = 1
 png(file.path(out_dir, "interviews-by-opener.png"), h = 5 * ppi, w = 7 * ppi, res = ppi)
 
 # set graphics parameters
-par(mfrow = c(1,2), mar = c(2,2,1,1), oma = c(0,2,0,2), xaxs = "i", yaxs = "i", tcl = -0.15, mgp = c(2,0.35,0))
+par(mfrow = c(1,2), mar = c(2,1.5,1,0.5), oma = c(0,2,0,2), xaxs = "i", yaxs = "i", tcl = -0.15, mgp = c(2,0.35,0))
 
 # make barplot: total effort estimate by date
-mp = barplot(ests$total, ylim = c(0, max(ests$total, na.rm = TRUE)), las = 1, col = "grey75", space = 0.1)
+mp = barplot(ests$total, ylim = c(0, max(ests$total, na.rm = TRUE)), las = 1, col = "grey75", space = 0)
 usr = par("usr")
 segments(usr[1], usr[3], usr[2], usr[3], xpd = TRUE)
-axis(side = 1, at = mp, labels = dates, las = 2)
+axis(side = 1, at = mp, labels = dates, las = 2, cex.axis = 0.8)
 
 # bake stacked barplot: proportion of effort in each stratum by date
-par(mar = c(2,1,1,2))
-mp = barplot(p, beside = F, ylim = c(0,1), col = c("grey45", "grey65", "grey85", "grey95"), space = 0.1, yaxt = "n", xaxt = "n")
+par(mar = c(2,0.5,1,1.5))
+mp = barplot(p, beside = F, ylim = c(0,1), col = c("grey45", "grey65", "grey85", "grey95"), space = 0, yaxt = "n", xaxt = "n")
 usr = par("usr")
 axis(side = 4, at = seq(0,1,0.2), labels = seq(0,1,0.2), las = 2)
 segments(usr[1], usr[3], usr[2], usr[3], xpd = TRUE)
-axis(side = 1, at = mp, labels = dates, las = 2)
+axis(side = 1, at = mp, labels = dates, las = 2, cex.axis = 0.8)
 
 # label the bar with stratum
 text(x = mp[label_bar_BBH], y = p[1,label_bar_BBH]/2, labels = "BBH", font = 2, srt = 90)
@@ -190,8 +190,8 @@ text(x = mp[label_bar_CBM], y = (sum(p[1:2,label_bar_CBM]) + p[1,label_bar_CBM])
 text(x = mp[label_bar_FC], y = (sum(p[1:3,label_bar_FC]) + sum(p[1:2,label_bar_FC]))/2, labels = "FC", font = 2, srt = 90)
 
 # add yaxis labels
-mtext(side = 2, outer = T, line = 0.5, "Total Interviews")
-mtext(side = 4, outer = T, line = 0.5, "Proportion of Interviews by Source")
+mtext(side = 2, outer = T, line = 0.75, "Total Interviews")
+mtext(side = 4, outer = T, line = 0.75, "Proportion of Interviews by Source")
 
 # close the graphics device
 dev.off(); file.show(file.path(out_dir, "interviews-by-opener.png"))
@@ -202,43 +202,57 @@ dev.off(); file.show(file.path(out_dir, "interviews-by-opener.png"))
 interviews = readRDS(file.path(in_dir, "all_interview_data.rds"))
 
 # specify which variables to make histograms for
-hist_variables = c("total_salmon", "chinook", "chum", "sockeye", "trip_start", "trip_duration", "soak_duration", "p_chinook")
+hist_variables = c("chinook", "chum", "sockeye", "coho", "trip_start", "trip_duration", "soak_duration", "p_chinook")
 
 # format the dates to make histograms for
 dates = unique(lubridate::date(interviews$trip_start))
 dates = dates[!is.na(dates)]
 
 # a function to make one histogram file for one opener
+
+plot_date = "2023-07-05"
+
+select_gear = function(plot_date) {
+  interview_data_sub = subset(interviews, lubridate::date(trip_start) == plot_date)
+  ifelse(as.character(plot_date) %in% set_only_openers | all(interview_data_sub$gear == "set"), "set", "drift")
+}
+
 hist_function = function(plot_date) {
 
   cat("\rMaking Histogram File for", as.character(plot_date))
 
-  gear = ifelse(as.character(plot_date) %in% set_only_openers, "set", "drift")
+  gear = select_gear(plot_date)
+  interview_data_sub = subset(interviews, lubridate::date(trip_start) == plot_date)
+  interview_data_sub$coho[is.na(interview_data_sub$coho)] = 0
 
   file_date = KuskoHarvUtils::file_date(plot_date)
   file_name = file.path(out_dir, paste0("histograms_", gear, "_", file_date, ".png"))
 
   png(file_name, h = 8 * ppi, w = 5 * ppi, res = ppi)
   KuskoHarvEst::make_histograms(
-    interview_data = subset(interviews, lubridate::date(trip_start) == plot_date),
+    interview_data = interview_data_sub,
     gear = gear,
     variables = hist_variables,
     mfrow = c(4,2), n_bins = 10
   )
   dev.off(); file.show(file_name)
 }
-# KuskoHarvEst::make_histograms()
 
 # loop through dates, making the histogram figure file for each
 junk = sapply(dates, hist_function); rm(junk)
 
+# which gear types are shown for each opener's plot
+do.call(rbind, lapply(dates, function(d) data.frame(date = d, gear_shown = select_gear(d))))
+
 ##### HARVEST BY SPECIES AND OPENER #####
+
 
 # read in the output
 ests = read.csv(file.path(in_dir, "all-harvest-summaries.csv"))
 
 # subset only estimates that will be plotted
 ests = subset(ests, stratum == "total" & gear == "total" & date != "total" & species != "total")
+ests$species = factor(ests$species, levels = c("chinook", "chum", "sockeye", "coho"))
 
 # format the dates
 dates = unique(KuskoHarvUtils::basic_date(ests$date))
@@ -261,18 +275,18 @@ par(mar = c(2,4,2,2), tcl = -0.15, mgp = c(2,0.35,0))
 
 # make the grouped barplot
 mp = barplot(means, beside = TRUE, ylim = c(0, max(uprs, na.rm = TRUE) * 1.05),
-             names.arg = dates, yaxt = "n", col = cols)
+             names.arg = rep("", length(dates)), yaxt = "n", col = cols)
 
 # handle axes
 usr = par("usr")
 segments(usr[1], usr[3], usr[2], usr[3], xpd = T)
-axis(side = 1, at = mp[2,], labels = FALSE)
+axis(side = 1, at = colSums(mp[2:3,])/2, labels = dates, las = 2)
 at_y = axisTicks(usr[3:4], log = FALSE)
 axis(side = 2, at = at_y, labels = prettyNum(at_y, big.mark = ",", scientific = FALSE), las = 2)
 mtext(side = 2, line = 3, "Harvest")
 
 # add error bars (suppress warnings about NA length arrows)
-suppressWarnings(arrows(mp, lwrs, mp, uprs, code = 3, angle = 90, length = 0.05))
+suppressWarnings(arrows(mp, lwrs, mp, uprs, code = 3, angle = 90, length = 0.01))
 
 # add a legend
 legend("topleft", legend = spp, pch = 22, pt.cex = 2.5, pt.bg = cols, bty = "n")
@@ -281,6 +295,7 @@ legend("topleft", legend = spp, pch = 22, pt.cex = 2.5, pt.bg = cols, bty = "n")
 dev.off(); file.show(file.path(out_dir, "harvest-by-species-and-opener.png"))
 
 ##### TOTAL HARVEST BY SPECIES #####
+
 # read in the output
 ests = read.csv(file.path(in_dir, "all-harvest-summaries.csv"))
 
@@ -341,7 +356,7 @@ mp = barplot(means, beside = TRUE, ylim = c(0, max(uprs, na.rm = TRUE) * 1.05),
 # handle axes
 usr = par("usr")
 segments(usr[1], usr[3], usr[2], usr[3], xpd = T)
-axis(side = 1, at = mp[2,], labels = FALSE)
+axis(side = 1, at = colSums(mp[2:3,])/2, labels = FALSE)
 at_y = axisTicks(usr[3:4], log = FALSE)
 axis(side = 2, at = at_y, labels = prettyNum(at_y, big.mark = ",", scientific = FALSE), las = 2)
 mtext(side = 2, line = 3, "Harvest")
